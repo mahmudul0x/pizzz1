4009fb874edefc661edb8b242a2e61032701fd36
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2026-05-08 11:11:04 +0600

    jjjjjjjjjjjjjjjjjjjjjj

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 // @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
-// or the app will break with duplicate plugins:
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
